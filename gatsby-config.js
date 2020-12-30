1563a68a3533e2c56c161b8acf44d772f449070a
SpaceMan <tatendachris@gmail.com>
2020-12-30 15:22:44 +0200

    Redesign

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     titleTemplate: "%s · Software Engineering Team",
     description:"We are Team Tachom, a Software Engineering team based in Zimbabwe. We develop creative but simple to use software solutions without compromising on security and we are looking forward to working with you on your next big project.",
     url: "https://tachom.co.zw", // No trailing slash allowed!
-    image: "/images/logo.ppg",
+    image: "/images/logo.png",
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -24,9 +24,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-2E7QZKF94H", // Google Analytics / GA
+          "G-2E7QZKF94H"
         ],
       },
     },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,14 @@
 import * as React from "react"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
 import HeaderTop from "../components/HeaderTop"
-import SEO from "../components/seo"
-import ServicesSection from "../components/Services"
-import TechnologyStackSection from "../components/TechnologyStackSection"
+import SEO from "../components/Services"
 import "../utils/styles.css"
-// markup
+
 const IndexPage = () => {
   return (
     <>
       <SEO/>
       <div className='container pt-15 mx-auto max-w-5xl text-gray-900'>
         <HeaderTop />
-        <Header />
-        <ServicesSection />
       </div>
     </>
     
