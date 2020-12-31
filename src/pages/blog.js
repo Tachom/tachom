6fd795deb86bdf5c958ece12771da57b1404624e
tate2301 <tatendachris@gmail.com>
2020-12-31 23:00:35 +0200

    :Redesign

---
+++ src/pages/blog.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import SEO from "../components/seo"
+
+
+class Blog extends React.Component {
+  render() {
+    const { data, navigate, location } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMdx.edges
+    const localSearchBlog = data.localSearchBlog
+
+    return (
+      <>
+        <SEO title="All posts" />
+        <SearchPosts
+          posts={posts}
+          localSearchBlog={localSearchBlog}
+          navigate={navigate}
+          location={location}
+        />
+
+      </>
+    )
+  }
+}
+
+export default Blog
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    localSearchBlog {
+      index
+      store
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
