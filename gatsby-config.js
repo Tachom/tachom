b80a8b8086b62a94e0d893954e68b9b6e0748cca
tate2301 <tatendachris@gmail.com>
2020-12-31 21:55:52 +0200

    Added Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
+    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-sharp",
     "gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
